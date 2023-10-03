eb24bdd2502a4a3335f15f22d1d826de857d9cd6
Mosteau <thibaut.mosteau@lilo.org>
2023-10-03 10:45:14 +0200

    envoie travail

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk" 
+
+console.log(chalk.blue("bonjour"))
+
+console.log(chalk.blue("Titouan"))
+console.log(chalk.red("Nadine"))
+console.log(chalk.green("Charles"))
+console.log(chalk.white("Thibaut"))
